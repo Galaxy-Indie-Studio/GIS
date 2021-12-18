d33009ff03ee9e125b51394c36a110a29df46e12
BigTallahasee <85016240+BigTallahasee@users.noreply.github.com>
2021-12-18 07:05:29 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-
-
-document.getElementById("").onclick = function(){
-    var loginUsername = document.getElementById("Login").value;
-
-    console.log("Hello",loginUsername);
-
-}
+CREATETABLE INTO users ()
---
+++ user.sql
@@ -0,0 +1,7 @@
+--CREATE TABLE USERS (username TEXT NOT NULL PRIMARY KEY,
+--password NOT NULL, email NOT NULL);
+
+INSERT INFO users(username,password,email)
+VALUES("jack", "mypassword", "jack@gmail.com");
+
+SELECT * FROM users
